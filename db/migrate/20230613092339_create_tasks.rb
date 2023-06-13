9b279e072648990b06c2a0830905c48333c69946
Benjamin <b.leray35@gmail.com>
2023-06-13 11:31:38 +0200

    add db setup and first simple model

---
+++ db/migrate/20230613092339_create_tasks.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# bundle exec rake db:create_migration\[create_tasks\]
+ROM::SQL.migration do
+  change do
+    create_table :tasks do
+      primary_key :id
+      column :title, :text, null: false
+      column :description, :text
+      column :due_date, :date
+    end
+  end
+end
+
+# then run by 
+# bundle exec rake db:migrate
+# HANAMI_ENV=test bundle exec rake db:migrate
