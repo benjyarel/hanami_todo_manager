d3f972a151c08a38637b63dbf099e748ebe116bd
Benjamin <b.leray35@gmail.com>
2023-06-29 06:14:44 +0200

    add spec file

--- app/actions/tasks/create.rb
+++ app/actions/tasks/create.rb
@@ -9,7 +9,7 @@ module HanamiTodoManager
       params do
         required(:title).value(:string)
         optional(:description).value(:string)
-        required(:due_date).value(:date)
+        optional(:due_date).value(:date)
       end
 
         def handle(request, response)
---
+++ spec/actions/tasks/create_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+RSpec.describe HanamiTodoManager::Actions::Tasks::Create, type: [:request, :database] do
+
+ describe 'CREATE tasks/:id' do
+
+ end
+end
