b7f97d7b54242938c68fb41d4ec205dad74200cc
Benjamin <b.leray35@gmail.com>
2023-06-30 08:20:33 +0200

    refacto action to srp methods

--- app/actions/tasks/create.rb
+++ app/actions/tasks/create.rb
@@ -6,26 +6,39 @@ module HanamiTodoManager
       class Create < HanamiTodoManager::Action
       include Deps["persistence.rom"]
        
-      params do
-        required(:task).hash do
-          required(:title).value(:string)
-          optional(:description).value(:string)
-          optional(:due_date).value(:date)
+        params do
+          required(:task).hash do
+            required(:title).value(:string)
+            optional(:description).value(:string)
+            optional(:due_date).value(:date)
+          end
         end
-      end
 
         def handle(request, response)
-
           if request.params.valid?
-            task = rom.relations[:tasks].changeset(:create, request.params[:task]).commit
-            response.status = 201
-            response.body = task.to_json
-          else
+            task = create_task(request.params[:task])
 
-            response.status = 422
-            response.format = :json
-            response.body = request.params.errors.to_json
+            succes_response(task, response)
+          else
+            error_response(request, response)
           end
+        end
+
+        private
+
+        def create_task(task)
+          rom.relations[:tasks].changeset(:create, task).commit
+        end
+
+        def succes_response(task, response)
+          response.status = 201
+          response.body = task.to_json
+        end
+
+        def error_response(request, response)
+          response.status = 422
+          response.format = :json
+          response.body = request.params.errors.to_json
         end
       end
     end
