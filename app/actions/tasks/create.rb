ff72e455ddc1fdf9b4b2ba2876feda490c1332b5
Benjamin <b.leray35@gmail.com>
2023-06-30 08:13:22 +0200

    implement create logic

--- app/actions/tasks/create.rb
+++ app/actions/tasks/create.rb
@@ -7,13 +7,25 @@ module HanamiTodoManager
       include Deps["persistence.rom"]
        
       params do
-        required(:title).value(:string)
-        optional(:description).value(:string)
-        optional(:due_date).value(:date)
+        required(:task).hash do
+          required(:title).value(:string)
+          optional(:description).value(:string)
+          optional(:due_date).value(:date)
+        end
       end
 
         def handle(request, response)
-          p request.params
+
+          if request.params.valid?
+            task = rom.relations[:tasks].changeset(:create, request.params[:task]).commit
+            response.status = 201
+            response.body = task.to_json
+          else
+
+            response.status = 422
+            response.format = :json
+            response.body = request.params.errors.to_json
+          end
         end
       end
     end
